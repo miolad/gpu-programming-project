ace2016dba23affe0bf81024d8d49b7737c4d658
Davide Miola <davide.miola99@gmail.com>
2022-12-10 09:16:11 +0100

    viewer: disable build.rs for aarch64 targets

--- cuda-viewer/build.rs
+++ cuda-viewer/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    #[cfg(not(target_arch = "aarch64"))]
     cbindgen::Builder::new()
         .with_crate(std::env::var("CARGO_MANIFEST_DIR").unwrap())
         .with_language(cbindgen::Language::Cxx)
